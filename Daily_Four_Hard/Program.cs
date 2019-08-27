f4157ab9c112a6b25e6d340bde7e94b503597b1e
Wes Garbee <wgarbee@gmail.com>
2019-08-27 18:12:15 -0500

    initial commit - to revisit

---
+++ Daily_Four_Hard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Daily_Four_Hard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
